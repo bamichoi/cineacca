91041fb221f11919d479428c1a78c43cfa7afde0
beomseok.choi <cbs412@naver.com>
2021-12-07 13:22:51 +0900

    configuration postgresqul

--- config/settings.py
+++ config/settings.py
@@ -101,8 +101,12 @@ else:
 
     DATABASES = {
         "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
+            "ENGINE": "django.db.backends.postgresqul",
+            "HOST": os.environ.get("RDS_HOST"),
+            "NAME": os.environ.get("RDS_NAME"),
+            "USER": os.environ.get("RDS_USER"),
+            "PASSWORD": os.environ.get("RDS_PASSWORD"),
+            "PORT": "5432",
         }
     }
 
