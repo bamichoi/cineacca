2218e8496d3e021d0f697f7a0d6fda2b7b99bbbf
beomseok.choi <cbs412@naver.com>
2021-12-07 14:12:20 +0900

    change env var

--- config/settings.py
+++ config/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-!ye!r@^==hg233ok^@&4&3&7_xcdlf4kn+s+8j*8aa(wqyh!a!"
+SECRET_KEY = os.environ.get("DJANGO_SECRET")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get("DEBUG"))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".elasticbeanstalk.com"]
 
 
 # Application definition
