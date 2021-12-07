40b7cc462bb2ee7d2d3facce2a0b7c3fb41cc175
beomseok.choi <cbs412@naver.com>
2021-12-07 14:31:10 +0900

    error

--- config/settings.py
+++ config/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-!ye!r@^==hg233ok^@&4&3&7_xcdlf4kn+s+8j*8aa(wqyh!a!"
+SECRET_KEY = os.environ.get("DJANGO_SECRET")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG")
 
 ALLOWED_HOSTS = []
 
@@ -88,7 +88,7 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-if DEBUG is not False:
+if DEBUG:
 
     DATABASES = {
         "default": {
