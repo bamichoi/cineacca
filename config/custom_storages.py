104479f68c45a06b71bea9b94bf479f666790e0c
beomseok.choi <cbs412@naver.com>
2021-12-12 18:52:53 +0900

    change storage name

--- config/custom_storages.py
+++ config/custom_storages.py
@@ -1,8 +1,8 @@
 from storages.backends.s3boto3 import S3Boto3Storage
 
-class StaticStorages(S3Boto3Storage):
+class StaticStorage(S3Boto3Storage):
     location = "static/"
     file_overwrite = False
 
-class UploadStorages(S3Boto3Storage):
+class UploadStorage(S3Boto3Storage):
     location = "uploads/"
