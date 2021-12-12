4e3a7f57f784509d2e6bc5c5414d00ae0cbecb60
beomseok.choi <cbs412@naver.com>
2021-12-12 15:39:24 +0900

    static and upload storages

--- config/custom_storages.py
+++ config/custom_storages.py
@@ -1,1 +1,8 @@
 from storages.backends.s3boto3 import S3Boto3Storage
+
+class StaticStorages(S3Boto3Storage):
+    location = "static/"
+    file_overwrite = False
+
+class UploadStorages(S3Boto3Storage):
+    location = "uploads/"
