7941f290e68ca31474b66d71dda90f0c934b6a94
beomseok.choi <cbs412@naver.com>
2021-12-12 18:11:00 +0900

    modify command

--- config/settings.py
+++ config/settings.py
@@ -189,7 +189,6 @@ if not DEBUG :
     AWS_DEFAULT_ACL = "public-read"
     AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.eu-south-1.amazonaws.com"
     STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/static/"
-    AWS_REGION = "eu-south-1"
 
     sentry_sdk.init(
         dsn=os.environ.get("SENTRY_URL"),
