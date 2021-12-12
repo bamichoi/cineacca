531c527a04a7cad7f1d59ac0e8b1a81b6ab40285
beomseok.choi <cbs412@naver.com>
2021-12-12 14:54:26 +0900

    cofiguration static and media

--- config/settings.py
+++ config/settings.py
@@ -194,7 +194,6 @@ if not DEBUG :
 
     AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.eu-south-1.amazonaws.com"
     STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/static"
-    MEDIA_URL = ""
     
     sentry_sdk.init(
         dsn=os.environ.get("SENTRY_URL"),
