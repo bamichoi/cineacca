43c531a0fd8dc2ccac51a6d71e707f6ba27f1c05
beomseok.choi <cbs412@naver.com>
2021-12-12 16:03:27 +0900

    whyy

--- config/settings.py
+++ config/settings.py
@@ -180,6 +180,12 @@ EMAIL_FROM = "noreplycineacca@gmail.com"
 
 ## Sentry
 
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = "cineacca"
+AWS_DEFAULT_ACL = "public-read"
 
 if not DEBUG :
 
