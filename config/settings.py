603de3916b296e36b12b1bacb68cb9a5ed50b54b
beomseok.choi <cbs412@naver.com>
2021-12-12 15:51:30 +0900

    reset

--- config/settings.py
+++ config/settings.py
@@ -180,17 +180,17 @@ EMAIL_FROM = "noreplycineacca@gmail.com"
 
 ## Sentry
 
+DEFAULT_FILE_STORAGE = 'config.custom_storages.UploadStorages'
+STATICFILES_STORAGE = 'config.custom_storages.StaticStorages'
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = "cineacca"
+AWS_DEFAULT_ACL = "public-read"
+
 if not DEBUG :
 
-    DEFAULT_FILE_STORAGE = 'config.custom_storages.UploadStorages'
-    STATICFILES_STORAGE = 'config.custom_storages.StaticStorages'
-    AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
-    AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
-    AWS_STORAGE_BUCKET_NAME = "cineacca"
-    AWS_DEFAULT_ACL = "public-read"
-
-    AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.eu-south-1.amazonaws.com"
-    STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/static/"
+    """AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.eu-south-1.amazonaws.com"
+    STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/static/"""
     
     sentry_sdk.init(
         dsn=os.environ.get("SENTRY_URL"),
