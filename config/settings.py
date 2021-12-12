cec4de1221f97e77333a42d139d0c12dee739a8f
beomseok.choi <cbs412@naver.com>
2021-12-12 17:13:00 +0900

    reset

--- config/settings.py
+++ config/settings.py
@@ -192,7 +192,7 @@ if not DEBUG :
     AWS_STORAGE_BUCKET_NAME = "cineacca"
     AWS_DEFAULT_ACL = "public-read"
 
-AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.eu-south-1.amazonaws.com"
+    AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.eu-south-1.amazonaws.com"
     STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/static"
     
     sentry_sdk.init(
