6934796fa5301219ed85d50b5ee347aa5ffa089e
beomseok.choi <cbs412@naver.com>
2021-12-14 00:33:54 +0900

    retry

--- config/settings.py
+++ config/settings.py
@@ -184,10 +184,11 @@ if not DEBUG:
     AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
     AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
     AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
-    AWS_DEFAULT_ACL = "public-read"
+    AWS_DEFAULT_ACL = "public-read-write"
     AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.eu-south-1.amazonaws.com"
     STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/static/"
     AWS_S3_OBJECT_PARAMETERS = {"CacheControl": "max-age=86400"}
+    AWS_S3_REGION_NAME = "eu-south-1"
 
     sentry_sdk.init(
         dsn=os.environ.get("SENTRY_URL"),
