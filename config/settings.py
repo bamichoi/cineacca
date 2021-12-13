3d7fe6f476c34e6a15c3afa14e22df3da170c863
beomseok.choi <cbs412@naver.com>
2021-12-13 23:22:51 +0900

    reset

--- config/settings.py
+++ config/settings.py
@@ -184,7 +184,7 @@ if not DEBUG:
     AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
     AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
     AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
-    AWS_DEFAULT_ACL = "public-read-write"
+    AWS_DEFAULT_ACL = "public-read"
     AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.eu-south-1.amazonaws.com"
     STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/static/"
     AWS_S3_OBJECT_PARAMETERS = {"CacheControl": "max-age=86400"}
