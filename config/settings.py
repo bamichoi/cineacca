f34931f34f73f5936c4cdcb2931353ea223ec10d
beomseok.choi <cbs412@naver.com>
2021-12-19 14:15:51 +0900

    add email port

--- config/settings.py
+++ config/settings.py
@@ -180,6 +180,7 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_HOST_USER = os.environ.get("ADMIN_GMAIL")
 EMAIL_HOST_PASSWORD = os.environ.get("ADMIN_GMAIL_PASSWORD")
+EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_FROM = "noreplycineacca@gmail.com"
 
