8a82b8f083d513c99ec5d50150f522b486cd146f
beomseok.choi <cbs412@naver.com>
2021-12-21 17:28:59 +0900

    try

--- config/settings.py
+++ config/settings.py
@@ -191,7 +191,24 @@ EMAIL_FROM = "noreplycineacca@gmail.com"
 
 if DEBUG is False :
 
-   
+    # the json credentials stored as env variable
+    json_str = os.environ.get('GOOGLE_APPLICATION_CREDENTIALS')
+    # project name
+    gcp_project = os.environ.get('GCP_PROJECT') 
+
+    # generate json - if there are errors here remove newlines in .env
+    json_data = json.loads(json_str)
+    # the private_key needs to replace \n parsed as string literal with escaped newlines
+    json_data['private_key'] = json_data['private_key'].replace('\\n', '\n')
+
+    # use service_account to generate credentials object
+    credentials = service_account.Credentials.from_service_account_info(
+        json_data)
+
+    # pass credentials AND project name to new client object (did not work wihout project name)
+    storage_client = storage.Client(
+        project=gcp_project, credentials=credentials)
+
     """GS_CREDENTIALS = service_account.Credentials.from_service_account_file(
     os.path.join(BASE_DIR, 'credential.json')
 )"""
