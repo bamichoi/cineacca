efc6fbcea9b388f585b11766a489cf013bb4f7e8
beomseok.choi <cbs412@naver.com>
2021-12-24 21:00:03 +0900

    try

--- videoarts/middleware.py
+++ videoarts/middleware.py
@@ -7,6 +7,5 @@ class CorsIsolation:
         response = self.get_response(request)
         response.headers["Access-Control-Allow-Origin"] = "*" # 필요없는 것들 지워주기
         response.headers["Cross-Origin-Embedder-Policy"] = "require-corp"
-        response.headers["Cross-Origin-Opener-Policy"] = "same-origin" 
         response.headers["Cross-Origin-Resource-Policy"] = "cross-origin"
         return response
