2cbb265dfa71eb030a75e583ece287940b234b09
beomseok.choi <cbs412@naver.com>
2021-12-24 18:47:32 +0900

    add access controll aloow origin

--- videoarts/middleware.py
+++ videoarts/middleware.py
@@ -5,6 +5,7 @@ class CorsIsolation:
 
     def __call__(self, request):
         response = self.get_response(request)
+        response.headers["Access-Control-Allow-Origin'"] = "*"
         response.headers["Cross-Origin-Embedder-Policy"] = "require-corp"
         response.headers["Cross-Origin-Opener-Policy"] = "same-origin" 
         return response
