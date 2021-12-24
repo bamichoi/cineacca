9047bb46bfd64dd3d2c29fe80b21ac832e01d579
beomseok.choi <cbs412@naver.com>
2021-12-24 21:14:58 +0900

    whyyy

--- config/settings.py
+++ config/settings.py
@@ -68,6 +68,10 @@ MIDDLEWARE = [
     
 ]
 
+CORS_ORIGIN_ALLOW_ALL = True
+
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 
 
--- videoarts/middleware.py
+++ videoarts/middleware.py
@@ -5,6 +5,7 @@ class CorsIsolation:
 
     def __call__(self, request):
         response = self.get_response(request)
+        response.headers["Cross-Origin-Resource-Policy"] = "cross-origin" 
         response.headers["Cross-Origin-Embedder-Policy"] = "require-corp"
         response.headers["Cross-Origin-Opener-Policy"] = "same-origin" 
         return response
