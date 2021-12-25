994c0707fdf6511777ba01a25d03e2b71ffc0e94
beomseok.choi <cbs412@naver.com>
2021-12-25 12:06:29 +0900

    retry

--- core/middleware.py
+++ core/middleware.py
@@ -5,7 +5,7 @@ class CorsHeaders:
 
     def __call__(self, request):
         response = self.get_response(request)
-        response.headers["Access-Control-Allow-Origin"] = ["https://cineacca.com", "torage.googleapis.com/cineacca_bucket"]
+        response.headers["Access-Control-Allow-Origin"] = ["https://cineacca.com", "storage.googleapis.com/cineacca_bucket"]
         response.headers["Access-Control-Allow-Methods"] = ["GET", "PUT", "DELETE", "OPTION"]
         response.headers["Cross-Origin-Resource-Policy"] = "cross-origin" 
         response.headers["Cross-Origin-Embedder-Policy"] = "require-corp"
