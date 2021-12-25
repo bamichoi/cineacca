cc5228921fe66c92d69db968cd815bf6f805e80b
beomseok.choi <cbs412@naver.com>
2021-12-25 16:51:36 +0900

    tset

--- config/settings.py
+++ config/settings.py
@@ -216,7 +216,6 @@ EMAIL_FROM = "noreplycineacca@gmail.com"
 if DEBUG is False :
 
     GOOGLE_APPLICATION_CREDENTIALS = os.environ.get("GOOGLE_APPLICATION_CREDENTIALS")
-    STATICFILES_STORAGE = "config.custom_storages.UploadStorage"
     DEFAULT_FILE_STORAGE = "config.custom_storages.UploadStorage"
     GS_BUCKET_NAME = "cineacca_bucket"
     GS_PROJECT_ID = os.environ.get("GS_PROJECT_ID")
@@ -238,3 +237,4 @@ if DEBUG is False :
     #STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
     
     django_heroku.settings(locals())    
+    STATICFILES_STORAGE = "config.custom_storages.UploadStorage"
