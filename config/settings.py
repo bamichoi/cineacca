9d39c28464b1810dbfdc973424b7cc65c04ef2fd
beomseok.choi <cbs412@naver.com>
2021-12-26 11:24:29 +0900

    setting sengrid

--- config/settings.py
+++ config/settings.py
@@ -212,7 +212,7 @@ EMAIL_HOST_USER = os.environ.get("ADMIN_GMAIL")
 EMAIL_HOST_PASSWORD = os.environ.get("ADMIN_GMAIL_PASSWORD")
 EMAIL_PORT = 587    
 EMAIL_USE_TLS = True
-EMAIL_FROM = "noreplycineacca@gmail.com"
+EMAIL_FROM = "noreply@cineacca.com"
 
 #getting credential
 
