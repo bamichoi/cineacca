7b8cc05e0f82b41d097deebdece5b6d1db0c2ef8
beomseok.choi <cbs412@naver.com>
2021-12-30 16:09:48 +0900

    video thumbnail

--- assets/js/movies/videoPlayer.js
+++ assets/js/movies/videoPlayer.js
@@ -1,13 +1,22 @@
 import videojs from "video.js";
 
 const movieInfo = document.querySelector(".detail_movieInfo");
-const thumbnail = movieInfo.dataset.thumb;
+const thumbnailUrl = movieInfo.dataset.thumb;
+const videoPlayer = document.querySelector("#videoPlayer_html5_api")
+const thumbnailImg = document.createElement('img');
+
+thumbnailImg.classList.add("vjs-poster", "vjs-hidden");
+thumbnailImg.setAttribute("tabindex", "-1");
+thumbnailImg.setAttribute("aria-disabled", "false");
+thumbnailImg.setAttribute("crossorigin", "anonymous");
+
+console.log(thumbnailImg)
+
 
 const options = {
     controls: true,
     preload:"metadata",
     fluid:true,
-    //poster:thumbnail
 };
 //to do: breakpoints (responsive option)
 
