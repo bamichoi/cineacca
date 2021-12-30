c01b5bf625e0f8f284bf7d031a1330d17e19026c
beomseok.choi <cbs412@naver.com>
2021-12-30 16:30:22 +0900

    poster

--- assets/js/movies/videoPlayer.js
+++ assets/js/movies/videoPlayer.js
@@ -2,21 +2,12 @@ import videojs from "video.js";
 
 const movieInfo = document.querySelector(".detail_movieInfo");
 const thumbnailUrl = movieInfo.dataset.thumb;
-const videoPlayer = document.querySelector("#videoPlayer_html5_api")
-const thumbnailImg = document.createElement('img');
-
-thumbnailImg.classList.add("vjs-poster", "vjs-hidden");
-thumbnailImg.setAttribute("tabindex", "-1");
-thumbnailImg.setAttribute("aria-disabled", "false");
-thumbnailImg.setAttribute("crossorigin", "anonymous");
-
-console.log(thumbnailImg)
-
 
 const options = {
     controls: true,
     preload:"metadata",
     fluid:true,
+    poster:thumbnailUrl
 };
 //to do: breakpoints (responsive option)
 
--- core/middleware.py
+++ core/middleware.py
@@ -7,7 +7,9 @@ class CorsHeaders:
         response = self.get_response(request)
         response.headers["Access-Control-Allow-Origin"] = ["https://cineacca.com", "https://storage.googleapis.com/cineacca_bucket"]
         response.headers["Access-Control-Allow-Methods"] = ["GET", "PUT", "DELETE", "OPTION"]
-        response.headers["Cross-Origin-Resource-Policy"] = "cross-origin" 
+        response.headers["Cross-Origin-Resource-Policy"] = "cross-origin"
+        response.header["Access-Control-Allow-Credential"] = "true"
+        response.header["Accept"] = "*/*"
         response.headers["Cross-Origin-Embedder-Policy"] = "require-corp"
         response.headers["Cross-Origin-Opener-Policy"] = "same-origin" 
         return response
