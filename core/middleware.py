a1e7461ff9063d5e235668d87bad9a8a7bd02a35
beomseok.choi <cbs412@naver.com>
2021-12-30 19:00:51 +0900

    roll back for fontawesome

--- core/middleware.py
+++ core/middleware.py
@@ -5,9 +5,9 @@ class CorsHeaders:
 
     def __call__(self, request):
         response = self.get_response(request)
-        response.headers["Access-Control-Allow-Origin"] = ["https://cineacca.com", "https://storage.googleapis.com/cineacca_bucket", "https://use.fontawesome.com"]
+        response.headers["Access-Control-Allow-Origin"] = ["https://cineacca.com", "https://storage.googleapis.com/cineacca_bucket"]
         response.headers["Access-Control-Allow-Methods"] = ["GET", "PUT", "DELETE", "OPTION"]
         response.headers["Cross-Origin-Resource-Policy"] = "cross-origin" 
-        response.headers["Cross-Origin-Embedder-Policy"] = "credentialless"
+        response.headers["Cross-Origin-Embedder-Policy"] = "require-corp"
         response.headers["Cross-Origin-Opener-Policy"] = "same-origin" 
         return response
