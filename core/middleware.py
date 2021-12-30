cef8318e885eaadf4ad90ad28aa22ffc4085926d
beomseok.choi <cbs412@naver.com>
2021-12-30 16:33:09 +0900

    get back

--- core/middleware.py
+++ core/middleware.py
@@ -8,7 +8,6 @@ class CorsHeaders:
         response.headers["Access-Control-Allow-Origin"] = ["https://cineacca.com", "https://storage.googleapis.com/cineacca_bucket"]
         response.headers["Access-Control-Allow-Methods"] = ["GET", "PUT", "DELETE", "OPTION"]
         response.headers["Cross-Origin-Resource-Policy"] = "cross-origin"
-        response.header["Access-Control-Allow-Credential"] = "true"
         response.header["Accept"] = "*/*"
         response.headers["Cross-Origin-Embedder-Policy"] = "require-corp"
         response.headers["Cross-Origin-Opener-Policy"] = "same-origin" 
