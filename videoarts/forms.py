ddb232313164d0fdfa279d8428fa91ef5b5bbc61
beomseok.choi <cbs412@naver.com>
2022-01-01 22:24:48 +0900

    crf30 50mb compressione passing

--- videoarts/forms.py
+++ videoarts/forms.py
@@ -120,7 +120,6 @@ class VideoArtUploadForm(forms.ModelForm):
     def save(self, *args, **kwargs):
         videoart = super().save(commit=False)
         video = self.cleaned_data.get("video")
-        print(video)
         video_path = video.temporary_file_path()  
         get_duration =  subprocess.check_output(['ffprobe', '-i', f'{video_path}', '-show_entries', 'format=duration', '-v', 'quiet', '-of', 'csv=%s' % ("p=0")])
         duration = int(float(get_duration.decode('utf-8').replace("\n", ""))) 
