8da416a9938e862297fd0eba769274583aa5920d
beomseok.choi <cbs412@naver.com>
2022-03-13 10:13:13 +0900

    update readme

--- assets/js/movies/copyUrl.js
+++ assets/js/movies/copyUrl.js
@@ -4,7 +4,6 @@ const copyBtn = document.querySelector(".copy_link_btn");
 
 const handleClickCopyBtn = async (event) => {
   const currentUrl = window.location.href;
-  console.log(currentUrl);
   await navigator.clipboard.writeText(currentUrl);
   alert("l'URL di questo video è copiato!");
 };
